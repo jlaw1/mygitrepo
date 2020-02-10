9608397df43c862388b6b1ac54c0d744863ff6db
Julian Law <julian.law113@gmail.com>
2020-02-10 11:27:11 -0800

    tried to fix function with try/except block

--- myscript.py
+++ myscript.py
@@ -1,1 +1,10 @@
 # create file on github -> bring into local repo
+# in class example sps to be a 4x4 identity matrix
+
+
+import numpy as np 
+
+def my_func(matrix_1, matrix_2):
+	return matrix_1 * matrix_2
+
+my_matrix
